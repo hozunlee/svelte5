cd28475296e2baedc93b500649b667f544a18a18
hozunlee <ho2yahh@gmail.com>
2024-12-13 12:57:05 +0900

    feat : init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { Button } from '$lib/components/ui/button';
+
+	let count1 = $state(0);
+
+	let count = 0;
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<Button on:click={() => count++}>Click me +</Button>
+<Button on:click={() => count--}>Click me -</Button>
+
+{count}
